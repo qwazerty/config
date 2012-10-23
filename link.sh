b99c2ef4027e231d684a315451464ff2d521dad7
Kevin Houdebert <kevin@qwazerty.eu>
2012-10-23 22:01:14 +0200

    Update link

--- link.sh
+++ link.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 
 var=`pwd`
-rm -rf ~/.mybashrc ~/.vimrc ~/.tmux.conf
+rm -rf ~/.mybashrc ~/.vimrc ~/.tmux.conf ~/.i3
 ln -s $var/.mybashrc ~/.mybashrc
 ln -s $var/.vimrc ~/.vimrc
 ln -s $var/.tmux.conf ~/.tmux.conf
+ln -s $var/.i3 ~/.i3
