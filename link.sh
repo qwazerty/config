7a947f4079eab41ceb18b57f7dc58bde3ff53f77
Kevin Houdebert <kevin@qwazerty.eu>
2012-10-23 22:21:52 +0200

    Update all files

--- link.sh
+++ link.sh
@@ -2,7 +2,6 @@
 
 var=`pwd`
 rm -rf ~/.mybashrc ~/.vimrc ~/.tmux.conf ~/.i3
-ln -s $var/.mybashrc ~/.mybashrc
-ln -s $var/.vimrc ~/.vimrc
-ln -s $var/.tmux.conf ~/.tmux.conf
-ln -s $var/.i3 ~/.i3
+ln -s $var/dotmybashrc ~/.mybashrc
+ln -s $var/dotvimrc ~/.vimrc
+ln -s $var/doti3 ~/.i3
