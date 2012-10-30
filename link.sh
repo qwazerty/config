4c769da0cd4d5e395102026d6320522f983a7ad6
Kevin Houdebert <kevin@qwazerty.eu>
2012-10-30 11:11:43 +0100

    Add login file. Update linking

--- link.sh
+++ link.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 
 var=`pwd`
-rm -rf ~/.mybashrc ~/.vimrc ~/.tmux.conf ~/.i3
+rm -rf ~/.mybashrc ~/.vimrc ~/.i3 ~/.login
 ln -s $var/dotmybashrc ~/.mybashrc
 ln -s $var/dotvimrc ~/.vimrc
 ln -s $var/doti3 ~/.i3
+ln -s $var/dotlogin ~/.login
