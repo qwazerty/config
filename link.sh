822523335eb5e190c31eab4167818eb647996765
Kevin Houdebert <kevin@qwazerty.eu>
2012-11-05 15:58:49 +0100

    Add i3status config file

--- link.sh
+++ link.sh
@@ -1,7 +1,11 @@
 #! /bin/sh
 
 var=`pwd`
-rm -rf ~/.mybashrc ~/.vimrc ~/.i3 ~/.login
+rm -f ~/.mybashrc
+rm -f ~/.vimrc
+rm -rf ~/.i3
+rm -rf ~/.config/i3status
 ln -s $var/dotmybashrc ~/.mybashrc
 ln -s $var/dotvimrc ~/.vimrc
 ln -s $var/doti3 ~/.i3
+ln -s $var/dotconfig/i3status ~/.config/i3status
