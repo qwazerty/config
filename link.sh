d5b7ab9d4c827e6e10ebbe20398f3174d3e8ec3f
Kevin Houdebert <kevin@qwazerty.eu>
2012-11-06 16:43:14 +0100

    Update conf files

--- link.sh
+++ link.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 var=`pwd`
-rm -rf ~/.mybashrc ~/.vimrc ~/.i3 ~/.login
+rm -rf ~/.mybashrc ~/.vimrc ~/.i3
 ln -s $var/dotmybashrc ~/.mybashrc
 ln -s $var/dotvimrc ~/.vimrc
 ln -s $var/doti3 ~/.i3
