d871976f339f88e4dc61e0f8cefbea9e645bb959
qwazerty <precursor.qwaz@hotmail.fr>
2012-12-07 15:26:00 +0000

    Update zlock

--- doti3/zlock.sh
+++ doti3/zlock.sh
@@ -1,2 +1,2 @@
-i3lock -c 420042 || \
-    zlock -immed -text "Run little kitty. RUN!" -pwtext "XOXOXOXO"
+zlock -immed -text "Run little kitty. RUN!" -pwtext "XOXOXOXOXOXOXOXOXOXOXOX"
+     || i3lock -c 420042
