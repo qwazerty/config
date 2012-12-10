4ecdff64670ef0ea85156b33f726a222c3a3dbd2
Kevin Houdebert <kevin@qwazerty.eu>
2012-12-10 17:21:44 +0000

    Add project_gen scripts

--- link.sh
+++ link.sh
@@ -7,9 +7,11 @@ rm -f ~/.vimpagerrc
 rm -rf ~/.i3
 rm -rf ~/.vim
 rm -rf ~/.config/i3status
+rm -rf ~/.project_gen
 ln -s $var/dotmybashrc ~/.mybashrc
 ln -s $var/dotvimrc ~/.vimrc
 ln -s $var/dotvimpagerrc ~/.vimpagerrc
 ln -s $var/doti3 ~/.i3
 ln -s $var/dotvim ~/.vim
 ln -s $var/dotconfig/i3status ~/.config/i3status
+ln -s $var/dotproject_gen ~/.project_gen
