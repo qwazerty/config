61c906136101382594fe127816d259d11a34dec2
qwazerty <precursor.qwaz@hotmail.fr>
2013-02-08 16:23:30 +0000

    Update i3status and add keymap.sh (Thanks to Horgix)

---
+++ doti3/keymap.sh
@@ -0,0 +1,41 @@
+# HGXonf
+# by Alexis 'Horgix' Chotard
+# https://bitbucket.org/Horgix/
+
+# keymap.sh for SCRIPTS
+# Change keymap by looping through a given set of keymap (us and fr here)
+
+#!/bin/bash
+
+keymaps=('us_intl' 'us')
+
+layout=$(setxkbmap -query | grep "layout" | grep -Eo "[^ ]*$")
+variant=$(setxkbmap -query | grep "variant" | grep -Eo "[^ ]*$")
+current=""
+next=0
+loop=0
+keynum=${#keymaps}
+
+if [ -z $variant ]; then
+    current="$layout"
+else
+    current="$layout $variant"
+fi
+
+#echo "${keymaps[@]}"
+
+while [[ $loop < 2 ]]; do
+    for map in "${keymaps[@]}"; do
+        if [ $next == 1 ]; then
+            setxkbmap $map
+            notify-send -t 1 "Keymap Changed" "$map"
+            exit 0
+        fi
+        if [[ "$map" = "$current" ]]; then
+            next=1
+        fi
+    done
+    loop=$(($loop + 1))
+done
+
+# EOF
