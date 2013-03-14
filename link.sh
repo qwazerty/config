d3a9147d7465727c080f33d69edd94c79d398c8e
qwazerty <precursor.qwaz@hotmail.fr>
2013-03-14 10:58:02 +0100

    Cleaned up link script

--- link.sh
+++ link.sh
@@ -1,17 +1,10 @@
 #! /bin/sh
 
 var=`pwd`
-rm -f ~/.mybashrc
-rm -f ~/.vimrc
-rm -f ~/.vimpagerrc
-rm -rf ~/.i3
-rm -rf ~/.vim
-rm -rf ~/.config/i3status
-rm -rf ~/.project_gen
-ln -s $var/dotmybashrc ~/.mybashrc
-ln -s $var/dotvimrc ~/.vimrc
-ln -s $var/dotvimpagerrc ~/.vimpagerrc
-ln -s $var/doti3 ~/.i3
-ln -s $var/dotvim ~/.vim
-ln -s $var/dotconfig/i3status ~/.config/i3status
-ln -s $var/dotproject_gen ~/.project_gen
+ln -fs $var/dotmybashrc ~/.mybashrc
+ln -fs $var/dotvimrc ~/.vimrc
+ln -fs $var/dotvimpagerrc ~/.vimpagerrc
+ln -fs $var/doti3 ~/.i3
+ln -fs $var/dotvim ~/.vim
+ln -fs $var/dotconfig/i3status ~/.config/i3status
+ln -fs $var/dotproject_gen ~/.project_gen
