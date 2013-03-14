7b74b48c6e87ccc7efe760c052d5ecffa5afa80d
Kevin Houdebert <kevin@qwazerty.eu>
2013-03-14 14:20:56 +0100

    Update prompt

--- dotzsh/prompt.sh
+++ dotzsh/prompt.sh
@@ -1,4 +1,12 @@
 # Zsh prompt
 #PS1="\n${rt}[\t] ${cR}[\u@\h:${cB}\w] \$(print_return \${ret_})\n${cR}\$${clr} "
 # This will set the default prompt to the walters theme
-prompt walters
+
+autoload -U colors && colors
+
+PROMPT="
+%{$fg_bold[red]%}[%T]%{$reset_color%}$fg_bold[magenta]-$reset_color\
+%{$fg_bold[green][%~]$reset_color%}
+%{$fg_bold[magenta]%}$%{$reset_color%} "
+RPROMPT="[%{$fg_bold[cyan]%}%n%{$fg_bold[red]%}@\
+%{$fg_bold[blue]%}%M%{$reset_color%}]"
