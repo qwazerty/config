59383bbf4a9519c11c2b5a651de5d3dabd69ea3c
Kevin Houdebert <kevin@qwazerty.eu>
2013-08-24 18:45:37 +0200

    Update global_link

--- global_link.sh
+++ global_link.sh
@@ -11,6 +11,8 @@ fi
 
 var=`pwd`
 mkdir -p ~/.config
+mkdir -p /etc/zsh
+rm -rf /etc/zsh/plugins
 rm -rf ~/.zsh
 rm -rf ~/.i3
 rm -rf ~/.vim
@@ -20,7 +22,7 @@ ln -fs $var/dotzshrc ~/.zshrc
 ln -fs $var/dotvimrc ~/.vimrc
 ln -fs $var/dotvimpagerrc ~/.vimpagerrc
 ln -fs $var/dotconfig/i3status ~/.config/i3status
-ln -fs $var/dotzsh ~/.zsh
+ln -fs $var/dotzsh /etc/zsh/plugins
 ln -fs $var/doti3 ~/.i3
 ln -fs $var/dotvim ~/.vim
 ln -fs $var/dotproject_gen ~/.project_gen
