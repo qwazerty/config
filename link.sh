9717640a3a5e3de9fcf00552bc1c58bdeebca913
qwazerty <precusor.qwaz@hotmail.fr>
2013-08-25 22:11:19 +0200

    Temp fix

--- link.sh
+++ link.sh
@@ -1,26 +1,18 @@
 #! /bin/sh
-echo "Use this if you want to install conf for all users (run as root)"
-read -p "Are you sure? (y/n) " REPLY
-case $REPLY in
-    y|Y ) ;;
-    * ) exit 1;;
-esac
-if [ `whoami` != "root" ]; then
-    echo "You are not root!"
-fi
 
 var=`pwd`
+
 mkdir -p ~/.config
+rm -rf ~/.zsh
 rm -rf ~/.zsh
 rm -rf ~/.i3
 rm -rf ~/.vim
 rm -rf ~/.project_gen
-ln -fs $var/dotmybashrc ~/.mybashrc
+ln -fs $var/dotzsh ~/.zsh
 ln -fs $var/dotzshrc ~/.zshrc
 ln -fs $var/dotvimrc ~/.vimrc
 ln -fs $var/dotvimpagerrc ~/.vimpagerrc
 ln -fs $var/dotconfig/i3status ~/.config/i3status
-ln -fs $var/dotzsh ~/.zsh
 ln -fs $var/doti3 ~/.i3
 ln -fs $var/dotvim ~/.vim
 ln -fs $var/dotproject_gen ~/.project_gen
