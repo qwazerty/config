0010ffbd75922100222a522a6abee18dfa8e5521
Kevin Houdebert <kevin@qwazerty.eu>
2013-08-27 15:57:20 +0200

    Still improving link script. Autogen zsh conf in either /etc/zsh/ or ~/.zsh/ depending on user permission

--- link.sh
+++ link.sh
@@ -1,26 +1,35 @@
 #! /bin/sh
 
-var=`pwd`
-
 mkdir -p ~/.config
 rm -rf ~/.zsh
-rm -rf ~/.zsh
+rm -rf ~/.config/i3status
 rm -rf ~/.i3
 rm -rf ~/.vim
 rm -rf ~/.project_gen
-rm -rf ~/.config/i3status
-ln -fs $var/dotzsh ~/.zsh
-ln -fs $var/dotzshrc ~/.zshrc
-ln -fs $var/dotvimrc ~/.vimrc
-ln -fs $var/dotvimpagerrc ~/.vimpagerrc
-ln -fs $var/dotconfig/i3status ~/.config/i3status
-ln -fs $var/doti3 ~/.i3
-ln -fs $var/dotvim ~/.vim
-ln -fs $var/dotproject_gen ~/.project_gen
-ln -fs $var/dotXresources ~/.Xresources
-ln -fs $var/dotxinitrc ~/.xinitrc
+rm -f ~/.zshrc
+rm -f ~/.vimrc
+rm -f ~/.vimpagerrc
+rm -f ~/.Xresources
+rm -f ~/.xinitrc
+
+cp -r dotzsh ~/.zsh
+cp -r dotconfig/i3status ~/.config/i3status
+cp -r doti3 ~/.i3
+cp -r dotvim ~/.vim
+cp -r dotproject_gen ~/.project_gen
+cp -r dotzshrc ~/.zshrc
+cp -r dotvimrc ~/.vimrc
+cp -r dotvimpagerrc ~/.vimpagerrc
+cp -r dotXresources ~/.Xresources
+cp -r dotxinitrc ~/.xinitrc
 
 if [ `whoami` = 'root' ]; then
     mkdir -p /etc/zsh/plugins/
     cp dotzsh/* /etc/zsh/plugins/ -r
+fi
+
+if [ -e /etc/zsh/plugins ]; then
+    sed -i -e 's/^ZSH=.*$/ZSH=\/etc\/zsh\/plugins\/oh-my-zsh/' ~/.zshrc
+else
+    sed -i -e 's/^ZSH=.*$/ZSH=~\/.zsh\/oh-my-zsh/' ~/.zshrc
 fi
