027e8b4b367956a8bcd1f8f8807c8a9d0e9697cf
Kevin Houdebert <kevin@qwazerty.eu>
2013-09-27 15:03:02 +0200

    [ZSH] Remove bold in prompt

--- dotzsh/prompt.zsh
+++ dotzsh/prompt.zsh
@@ -2,18 +2,18 @@ autoload -U colors && colors
 setopt prompt_subst
 
 function prompt_char {
-    echo -n "%(?.%{$fg_bold[blue]%}.%{$fg_bold[red]%})"
+    echo -n "%(?.%{$fg[blue]%}.%{$fg[red]%})"
     if [ $UID -eq 0 ]; then echo -n "#"; else echo -n $; fi
     echo -n "%{$reset_color%}"
 }
 
 function parse_git_branch {
     ref=$(git symbolic-ref HEAD 2> /dev/null) || return
-    echo "%{$fg_bold[red]%}(${ref#refs/heads/})%{$reset_color%} "
+    echo "%{$fg[red]%}(${ref#refs/heads/})%{$reset_color%} "
 }
 
 function clock {
-    echo "%(?.%{$fg_bold[blue]%}.%{$fg_bold[red]%})%*%{$reset_color%}"
+    echo "%(?.%{$fg[blue]%}.%{$fg[red]%})%*%{$reset_color%}"
 }
 
 function preexec {
@@ -29,10 +29,16 @@ function precmd {
 }
 
 function timer_show {
-    if [ "${TIMER_SHOW}" -ne 0 ]; then
-        echo "%{$fg_bold[cyan]%}${TIMER_SHOW}s "
+    timer=$TIMER_SHOW
+    if [ "$timer" -ne 0 ]; then
+        if [ "$timer" -ge 60 ]; then
+            tmp=$timer
+            timer=$(($tmp / 60)):$(($tmp % 60))
+            unset tmp
+        fi
+        echo "%{$fg[red]%}(%{$fg[cyan]%}$timer%{$fg[red]%}) "
     fi
 }
 
-export PROMPT='%(!.%{$fg_bold[red]%}.%{$fg_bold[green]%}%n@)%m %{$fg_bold[blue]%}%(!.%1~.%~) ${parse_git_branch}$(prompt_char)%{$reset_color%} '
+export PROMPT='%(!.%{$fg[red]%}.%{$fg[yellow]%}%n%{$fg[red]%}@)%m %{$fg[blue]%}%(!.%1~.%~) ${parse_git_branch}$(prompt_char)%{$reset_color%} '
 export RPROMPT='$(timer_show)$(parse_git_branch)$(clock)'
