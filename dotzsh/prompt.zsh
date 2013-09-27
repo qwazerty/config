547412b086ad689481ef203e0db2c9fffc53ce63
Kevin Houdebert <kevin@qwazerty.eu>
2013-09-27 13:44:49 +0200

    Add last command run time in prompt

--- dotzsh/prompt.zsh
+++ dotzsh/prompt.zsh
@@ -9,12 +9,30 @@ function prompt_char {
 
 function parse_git_branch {
     ref=$(git symbolic-ref HEAD 2> /dev/null) || return
-    echo %{$fg_bold[red]%}"("${ref#refs/heads/}")"%{$reset_color%}
+    echo "%{$fg_bold[red]%}(${ref#refs/heads/})%{$reset_color%} "
 }
 
 function clock {
     echo "%(?.%{$fg_bold[blue]%}.%{$fg_bold[red]%})%*%{$reset_color%}"
 }
 
+function preexec {
+    typeset -gi CALCTIME=1
+    typeset -gi CMDSTARTTIME=SECONDS
+}
+
+function precmd {
+    if (( CALCTIME )); then
+        typeset -gi TIMER_SHOW=SECONDS-CMDSTARTTIME
+    fi
+    typeset -gi CALCTIME=0
+}
+
+function timer_show {
+    if [ ${TIMER_SHOW} -ne 0 ]; then
+        echo "%{$fg_bold[cyan]%}${TIMER_SHOW}s "
+    fi
+}
+
 export PROMPT='%(!.%{$fg_bold[red]%}.%{$fg_bold[green]%}%n@)%m %{$fg_bold[blue]%}%(!.%1~.%~) ${parse_git_branch}$(prompt_char)%{$reset_color%} '
-export RPROMPT='$(parse_git_branch) $(clock)'
+export RPROMPT='$(timer_show)$(parse_git_branch)$(clock)'
