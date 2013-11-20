e80bc64ab498962542e6d4a98ffed8aedf266e3a
Kevin Houdebert <kevin@qwazerty.eu>
2013-11-21 00:53:18 +0100

    [MISC] Add gitconfig

--- link.sh
+++ link.sh
@@ -14,6 +14,7 @@ rm -f ~/.vimpagerrc
 rm -f ~/.Xresources
 rm -f ~/.xinitrc
 rm -f ~/.zcompdump
+rm -f ~/.gitconfig
 
 ln -s $PWD/dotzsh ~/.zsh
 ln -s $PWD/dotconfig/i3status ~/.config/i3status
@@ -25,3 +26,4 @@ ln -s $PWD/dotvimrc ~/.vimrc
 ln -s $PWD/dotvimpagerrc ~/.vimpagerrc
 ln -s $PWD/dotXresources ~/.Xresources
 ln -s $PWD/dotxinitrc ~/.xinitrc
+ln -s $PWD/dotgitconfig ~/.gitconfig
