8d45416f011d38b4727c5c0844c2480ea3e7417a
Kevin "qwazerty" Houdebert <precursor.qwaz@hotmail.fr>
2013-11-26 20:55:59 +0100

    [I3] Moved i3status conf

--- link.sh
+++ link.sh
@@ -2,12 +2,9 @@
 
 PWD=`pwd`
 
-mkdir -p ~/.config
 rm -rf ~/.zsh
-rm -rf ~/.config/i3status
 rm -rf ~/.i3
 rm -rf ~/.vim
-rm -rf ~/.project_gen
 rm -f ~/.zshrc
 rm -f ~/.vimrc
 rm -f ~/.vimpagerrc
@@ -17,10 +14,8 @@ rm -f ~/.zcompdump
 rm -f ~/.gitconfig
 
 ln -s $PWD/zsh ~/.zsh
-ln -s $PWD/config/i3status ~/.config/i3status
 ln -s $PWD/i3 ~/.i3
 ln -s $PWD/vim ~/.vim
-ln -s $PWD/project_gen ~/.project_gen
 ln -s $PWD/zshrc ~/.zshrc
 ln -s $PWD/vimrc ~/.vimrc
 ln -s $PWD/vimpagerrc ~/.vimpagerrc
