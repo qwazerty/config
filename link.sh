f83cedf0908895b6e2aa59c7ef6fd54c81bc0641
Kevin "qwazerty" Houdebert <precursor.qwaz@hotmail.fr>
2013-11-27 23:02:24 +0100

    [IRSSI][MISC] Add confs. And some other stuff

--- link.sh
+++ link.sh
@@ -5,6 +5,7 @@ PWD=`pwd`
 rm -rf ~/.zsh
 rm -rf ~/.i3
 rm -rf ~/.vim
+rm -rf ~/.irssi
 rm -f ~/.zshrc
 rm -f ~/.vimrc
 rm -f ~/.vimpagerrc
@@ -12,13 +13,16 @@ rm -f ~/.Xresources
 rm -f ~/.xinitrc
 rm -f ~/.zcompdump
 rm -f ~/.gitconfig
+rm -f ~/.tmux.conf
 
 ln -s $PWD/zsh ~/.zsh
 ln -s $PWD/i3 ~/.i3
 ln -s $PWD/vim ~/.vim
+ln -s $PWD/irssi ~/.irssi
 ln -s $PWD/zshrc ~/.zshrc
 ln -s $PWD/vimrc ~/.vimrc
 ln -s $PWD/vimpagerrc ~/.vimpagerrc
 ln -s $PWD/Xresources ~/.Xresources
 ln -s $PWD/xinitrc ~/.xinitrc
 ln -s $PWD/gitconfig ~/.gitconfig
+ln -s $PWD/tmux.conf ~/.tmux.conf
