ee6700a9c4d61d77a4fc18e51f26d735e31af06d
Kevin Houdebert <kevin@qwazerty.eu>
2013-12-11 22:34:40 +0100

    [ZSH] Fix TERM env in tmux

--- zsh/environment.zsh
+++ zsh/environment.zsh
@@ -17,8 +17,8 @@ export LANG="$LC_ALL"
 # Set TERMINAL for i3-sensible-terminal
 export TERMINAL=urxvt
 
-if [ -n "$SSH_CONNECTION" ]; then
-    export TERM=rxvt-unicode
+if [ -n "$TMUX" ]; then
+    export TERM=screen-256color
 fi
 
 # Set NNTP Server for epita newsgroups (slrn)
