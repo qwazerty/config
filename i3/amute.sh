c8a090ee53f89c56a2a6d6a756893cb192bffe49
Kevin "qwazerty" Houdebert <precursor.qwaz@hotmail.fr>
2014-01-19 18:21:45 +0000

    [I3] Refactor bindsym scripts into one file. Todo: allow mybindsym per binds

--- i3/amute.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-STATE=`amixer get Master | egrep 'Playback.*?\[o' | awk '{ print $6 }'`
-
-if [ $STATE == '[on]' ]; then
-    amixer set Master mute
-else
-    amixer set Master unmute
-fi
--- i3/touchpad.sh
+++
@@ -1,5 +0,0 @@
-if [ `synclient -l | grep TouchpadOff | gawk '{ print $3 }'` -eq 0 ]; then
-    synclient TouchpadOff=1;
-else
-    synclient TouchpadOff=0;
-fi
