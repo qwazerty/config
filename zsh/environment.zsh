1657d26f8d41ce6f42a310d50eaf74cf26ce0c8e
Kevin Houdebert <kevin@qwazerty.eu>
2014-09-30 03:57:01 +0200

    [ZSH] Add color to less

--- zsh/environment.zsh
+++ zsh/environment.zsh
@@ -33,5 +33,14 @@ export HISTSIZE=10000
 # History file size
 export SAVEHIST=10000
 
+# Color for less
+export LESS_TERMCAP_mb="\e[1;31m"
+export LESS_TERMCAP_md="\e[1;31m"
+export LESS_TERMCAP_me="\e[0m"
+export LESS_TERMCAP_se="\e[0m"
+export LESS_TERMCAP_so="\e[1;44;33m"
+export LESS_TERMCAP_ue="\e[0m"
+export LESS_TERMCAP_us="\e[1;32m"
+
 # Source ssh-agent
 [ -e ~/.ssh/ssh_${HOST}_${USER}.agent ] && . ~/.ssh/ssh_${HOST}_${USER}.agent
