bb73931b5b8f33a71e71b2e666034c2dbdff129b
Kevin Houdebert <kevin@qwazerty.eu>
2015-04-27 10:30:42 +0200

    [I3] Minor update to keymap.sh script

--- i3/keymap.sh
+++ i3/keymap.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 # HGXonf
 # by Alexis 'Horgix' Chotard
 # https://bitbucket.org/Horgix/
@@ -6,28 +5,21 @@
 # keymap.sh for SCRIPTS
 # Change keymap by looping through a given set of keymap (us and fr here)
 
-keymaps=('us_intl' 'us')
+#!/bin/bash
+
+keymaps=('us' 'us_intl')
 
 layout=$(setxkbmap -query | grep "layout" | grep -Eo "[^ ]*$")
-variant=$(setxkbmap -query | grep "variant" | grep -Eo "[^ ]*$")
-current=""
+current="$layout"
 next=0
 loop=0
 keynum=${#keymaps}
-
-if [ -z $variant ]; then
-    current="$layout"
-else
-    current="$layout $variant"
-fi
-
-#echo "${keymaps[@]}"
 
 while [[ $loop < 2 ]]; do
     for map in "${keymaps[@]}"; do
         if [ $next == 1 ]; then
             setxkbmap $map
-            notify-send -t 1 "Keymap Changed" "$map"
+            notify-send -t 2000 "Keymap Changed" "$map"
             exit 0
         fi
         if [[ "$map" = "$current" ]]; then
